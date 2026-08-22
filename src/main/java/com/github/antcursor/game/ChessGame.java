9ecc1c9626a85e743f67c6966f3d22fab035831e
AntCursor <antcursor@gmail.com>
2026-08-22 19:37:43 -0300

    game: added ChessGame class written by delocho

--- src/main/java/com/github/antcursor/game/ChessGame.java
+++ src/main/java/com/github/antcursor/game/ChessGame.java
@@ -1,15 +1,24 @@
 package com.github.antcursor.game;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.github.antcursor.board.Board;
+import com.github.antcursor.types.Color;
 import com.github.antcursor.types.Move;
 import com.github.antcursor.types.Position;
 
-/**
- * ChessGame
- */
 public class ChessGame {
-  // TODO: implement ChessGame
+  private Board board;
+  private Color turn;
+  private List<Move> moveHistory;
+
+  public ChessGame() {
+    this.board = new Board();
+    this.turn = Color.WHITE;
+    this.moveHistory = new ArrayList<>();
+  }
+
   public List<Move> getPossibleMoves(Position pos) {
     // TODO: implement method
     return null;
