77c1e6a46b1d66d5c9a0c1d7521651ab24df6601
AntCursor <antcursor@gmail.com>
2026-08-23 13:46:41 -0300

    move: moved SimpleMovement's case in MoveGenerator to helper method

--- src/main/java/com/github/antcursor/pieces/move/MoveGenerator.java
+++ src/main/java/com/github/antcursor/pieces/move/MoveGenerator.java
@@ -8,6 +8,7 @@ import com.github.antcursor.pieces.Piece;
 import com.github.antcursor.pieces.move.PieceMovement.PawnMovement;
 import com.github.antcursor.pieces.move.PieceMovement.SimpleMovement;
 import com.github.antcursor.types.Position;
+import com.github.antcursor.types.Color;
 import com.github.antcursor.types.Direction;
 
 /**
@@ -17,40 +18,48 @@ public class MoveGenerator {
   public static List<MoveCandidate> from(Position position, Board board) {
     Piece piece = board.getPiece(position);
 
-    List<MoveCandidate> moves = new ArrayList<>();
-
     switch (piece.type().movement()) {
       case SimpleMovement sp -> {
-
-        directions: for (Direction dir : sp.dirs()) {
-          Position to = position.addDirection(dir);
-          MoveType type = new MoveType.Normal();
-
-          boolean end = !sp.slide();
-          do {
-            if (!board.isOnBoard(to))
-              continue directions;
-
-            Piece target = board.getPiece(to);
-            if (target != null) {
-              if (target.color() == piece.color())
-                continue directions;
-
-              type = new MoveType.Capture(target.type());
-              end = true;
-            }
-
-            moves.add(
-                new MoveCandidate(
-                    position,
-                    to,
-                    type));
-
-            to = to.addDirection(dir);
-          } while (!end);
-        }
+        return simpleMoves(sp, piece.color(), position, board);
       }
       case PawnMovement pm -> {
+      }
+    }
+  }
+
+  private static List<MoveCandidate> simpleMoves(SimpleMovement sp, Color color, Position pos, Board board) {
+    List<MoveCandidate> moves = new ArrayList<>();
+
+    directions: for (Direction dir : sp.dirs()) {
+      Position to = pos.addDirection(dir);
+      MoveType type = new MoveType.Normal();
+
+      boolean end = !sp.slide();
+      do {
+        if (!board.isOnBoard(to))
+          continue directions;
+
+        Piece target = board.getPiece(to);
+        if (target != null) {
+          if (target.color() == color)
+            continue directions;
+
+          type = new MoveType.Capture(target.type());
+          end = true;
+        }
+
+        moves.add(
+            new MoveCandidate(
+                pos,
+                to,
+                type));
+
+        to = to.addDirection(dir);
+      } while (!end);
+    }
+
+    return moves;
+  }
 
       }
     }
