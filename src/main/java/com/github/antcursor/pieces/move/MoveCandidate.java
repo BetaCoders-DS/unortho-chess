d99e69a92e713eafaa7aaadd566a2e05823c7696
AntCursor <antcursor@gmail.com>
2026-08-22 21:01:38 -0300

    move: MoveType is now a sealed interface with records storing type specific data

--- src/main/java/com/github/antcursor/pieces/move/MoveCandidate.java
+++ src/main/java/com/github/antcursor/pieces/move/MoveCandidate.java
@@ -1,12 +1,9 @@
 package com.github.antcursor.pieces.move;
 
-import java.util.Optional;
-
-import com.github.antcursor.pieces.PieceType;
 import com.github.antcursor.types.Position;
 
 /**
  * MoveResult
  */
-public record MoveCandidate(Position from, Position to, MoveType type, Optional<PieceType> promoteTo) {
+public record MoveCandidate(Position from, Position to, MoveType type) {
 }
--- src/main/java/com/github/antcursor/pieces/move/MoveType.java
+++ src/main/java/com/github/antcursor/pieces/move/MoveType.java
@@ -1,14 +1,29 @@
 package com.github.antcursor.pieces.move;
 
-/**
+import java.util.Optional;
+
+import com.github.antcursor.pieces.PieceType;
+import com.github.antcursor.types.Position;
+
+/*
  * MoveType
- */
-public enum MoveType {
-  NONE,
-  NORMAL,
-  CAPTURE,
-  EN_PASSANT,
-  CASTLE_KING,
-  CASTLE_QUEEN,
-  PROMOTION
+*/
+public sealed interface MoveType {
+  public record Normal() implements MoveType {
+  }
+
+  public record Capture(PieceType captured) implements MoveType {
+  }
+
+  public record EnPassant(Position capturePos) implements MoveType {
+  }
+
+  public record CastleKing() implements MoveType {
+  }
+
+  public record CastleQueen() implements MoveType {
+  }
+
+  public record Promotion(PieceType promotedTo, Optional<PieceType> captured) {
+  }
 }
