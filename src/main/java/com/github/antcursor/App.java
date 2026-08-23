53c9380143aaf6c8b7b679f164bf3b348c1f7594
AntCursor <antcursor@gmail.com>
2026-08-23 18:35:04 -0300

    app: added Sketch creation and running to main

--- src/main/java/com/github/antcursor/App.java
+++ src/main/java/com/github/antcursor/App.java
@@ -1,13 +1,20 @@
 package com.github.antcursor;
 
+import processing.core.PApplet;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static void main(String[] args) {
+    Config app_config = new Config(
+        800, 600);
+
+    Sketch mainSketch = new Sketch(app_config);
+
+    PApplet.runSketch(
+        new String[] { Sketch.class.getName() },
+        mainSketch);
+  }
 }
